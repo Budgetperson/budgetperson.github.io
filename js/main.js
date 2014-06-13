6e7688cb91e1ef5515e2365615953b296197310c
Yash Aggarwal <yash0609@gmail.com>
2014-06-13 16:23:05 -0500

    new blog domain name, google analytics

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 google.load("feeds", "1");
-google.setOnLoadCallback(onGoogleReady);
+google.setOnLoadCallback(displayPosts);
 
-function onGoogleReady() {
-  var feed_url = "http://yash.svbtle.com/feed";
+function displayPosts() {
+  var feed_url = "http://blog.yashaggarwal.com/feed";
   var google_feed = new google.feeds.Feed(feed_url);
 
   google_feed.load(function(result) {
     for (var i = 0; i < 4 && typeof result.feed.entries[i] != 'undefined'; i++) {
       var entry = result.feed.entries[i];
+      // this is some crap JS but whatever
       var post_html = "<li><a href=\"" + entry.link + "\">" + entry.title + "</a></li>";
       $("#posts").append(post_html);
     }
